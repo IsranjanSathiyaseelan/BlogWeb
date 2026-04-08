fa24bbc9200182017143ea99375ff32371ffb290
isranjan <isranjansathiyaseelan@gmail.com>
2026-04-08 16:59:26 +0530

    arranged folder structures

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -3,14 +3,16 @@ import AboutPage from "../pages/AboutPage";
 import BlogPostPage from "../pages/BlogPostPage";
 import HomePage from "../pages/HomePage";
 import NotFoundPage from "../pages/NotFoundPage";
-import SignInPage from "../pages/SignInPage";
-import SignUpPage from "../pages/SignUpPage";
+import SignInPage from "../components/signin/SignInPage";
+import SignUpPage from "../components/signup/SignUpPage";
+import MyBlogs from "../pages/MyBlogs";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
+      <Route path="/myblog" element={<MyBlogs />} />
       <Route path="/signin" element={<SignInPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/blog/:slug" element={<BlogPostPage />} />
