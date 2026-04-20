29f91a6f5084d6b315066dadf35a399cb18effd9
isranjan <isranjansathiyaseelan@gmail.com>
2026-04-21 00:05:48 +0530

    myblogs file bug fixed

--- client/src/pages/MyBlogs.tsx
+++ client/src/pages/MyBlogs.tsx
@@ -1,4 +1,4 @@
-﻿import React, { useEffect, useRef, useState } from "react";
+﻿import React, { useRef, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import { useBlog } from "../context/blog/BlogContext";
