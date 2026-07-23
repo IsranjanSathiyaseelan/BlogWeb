30e7bd45f956fdfa4f2ee0cbf644888925168264
isranjan <isranjansathiyaseelan@gmail.com>
2026-07-23 20:44:05 +0530

    myblog bug fixed

--- client/src/pages/MyBlogs.tsx
+++ client/src/pages/MyBlogs.tsx
@@ -210,7 +210,7 @@ const MyBlogs = () => {
                 strokeWidth="2"
               >
                 <circle cx="12" cy="12" r="10" />
-                <line x1="12" y1="8" x2="12" y2="12" />
+                <line x1="12" y1="8" x2="12" />
                 <line x1="12" y1="16" x2="12.01" y2="16" />
               </svg>
               <span>{error}</span>
